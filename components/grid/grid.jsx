c5aa8e81f78ff0df6a4ad9bd5c72efa9602a4d21
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-07-02 12:36:01 +0200

    init files

---
+++ components/grid/grid.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import cn from "classnames";
+
+const Grid = ({ children, numberOfColumns }) => (
+  <div className={cn("grid", [`grid--columns-${numberOfColumns}`])}>
+    {React.Children.map(children, (child, index) => (
+      <div
+        key={index}
+        className={cn("grid__item", [`grid__item--number-${index + 1}`])}
+      >
+        {child}
+      </div>
+    ))}
+  </div>
+);
+
+Grid.propTypes = {
+  children: PropTypes.node.isRequired,
+  numberOfColumns: PropTypes.number
+};
+
+Grid.defaultProps = {
+  numberOfColumns: 4
+};
+
+export default Grid;
