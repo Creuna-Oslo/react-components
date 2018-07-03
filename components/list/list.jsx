b0dcad7133a9bfb4a63bcc9f35a406ecfd4f494e
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-07-03 15:40:22 +0200

    changed children proptype

--- components/list/list.jsx
+++ components/list/list.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import cn from 'classnames';
+import cn from "classnames";
 
 const List = ({ inline, ordered, className, children }) =>
   React.createElement(
-    ordered ? 'ol' : 'ul',
-    { className: cn('list', { 'list--inline': inline }, className) },
+    ordered ? "ol" : "ul",
+    { className: cn("list", { "list--inline": inline }, className) },
     React.Children.map(children, (child, index) => (
       <li key={index} className="list__item">
         {child}
@@ -18,10 +18,7 @@ List.propTypes = {
   inline: PropTypes.bool,
   ordered: PropTypes.bool,
   className: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.arrayOf(PropTypes.node)
-  ]).isRequired
+  children: PropTypes.node.isRequired
 };
 
 export default List;
