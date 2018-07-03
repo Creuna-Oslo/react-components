63dd3a7508a186ec769646d16f2b9b284acb48ec
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-07-03 13:46:15 +0200

    replaced magic scss classes with a jsx const containing required icon files, file is now inline styled on the element

--- components/icon/icon.jsx
+++ components/icon/icon.jsx
@@ -1,21 +1,29 @@
-import React from "react";
-import PropTypes from "prop-types";
-import cn from "classnames";
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 const sizes = {
-  small: "small",
-  medium: "medium",
-  large: "large"
+  small: 'small',
+  medium: 'medium',
+  large: 'large'
+};
+
+const availableIcons = {
+  menu: require('./hamburger.svg'),
+  'menu--active': require('./close.svg')
 };
 
 const Icon = ({ className, name, size }) =>
   !name ? null : (
-    <span className={cn("icon", `icon--${name}`, `icon--${size}`, className)} />
+    <span
+      className={cn('icon', `icon--${name}`, `icon--${size}`, className)}
+      style={{ backgroundImage: `url(${availableIcons[name]})` }}
+    />
   );
 
 Icon.propTypes = {
   className: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  name: PropTypes.string.isRequired,
+  name: PropTypes.oneOf(Object.keys(availableIcons)),
   size: PropTypes.oneOf(Object.keys(sizes).map(key => sizes[key]))
 };
 
