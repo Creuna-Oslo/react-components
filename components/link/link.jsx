a0ca415c5d807ddd4921d13839277fa3b80e1f83
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-07-03 15:56:28 +0200

    Removed ambiguous isEmail property, added rel="noopener noreferrer" attribute if link opens in new tab.

--- components/link/link.jsx
+++ components/link/link.jsx
@@ -12,28 +12,32 @@ const Link = ({
   href,
   children,
   theme,
-  isEmail,
   className,
   shouldOpenInNewTab
-}) => (
-  <a
-    className={cn("link", `link--theme-${theme}`, className)}
-    href={isEmail ? `mailto:${href}` : href}
-    target={shouldOpenInNewTab ? "_blank" : undefined}
-  >
-    <span className="link__content">{children || text || href}</span>
-  </a>
-);
+}) => {
+  const targetAttributes = shouldOpenInNewTab
+    ? {
+        target: "_blank",
+        rel: "noopener noreferrer"
+      }
+    : {};
+
+  return (
+    <a
+      className={cn("link", `link--theme-${theme}`, className)}
+      href={href}
+      {...targetAttributes}
+    >
+      <span className="link__content">{children || text || href}</span>
+    </a>
+  );
+};
 
 Link.propTypes = {
   text: PropTypes.string,
   href: PropTypes.string.isRequired,
-  isEmail: PropTypes.bool,
   className: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.arrayOf(PropTypes.node)
-  ]),
+  children: PropTypes.node,
   shouldOpenInNewTab: PropTypes.bool,
   theme: PropTypes.oneOf(Object.keys(themes).map(key => themes[key]))
 };
