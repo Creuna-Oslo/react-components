7df65b73f215227cdb74b84b26f8f36478f0e707
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-07-04 14:43:33 +0200

    removed assets, content is now piped through props (see readme example), css reduced and simplified

--- components/header/README.md
+++ components/header/README.md
@@ -1,10 +1,8 @@
 # DebounceRender
 
-Simple header component that support a logo, a list of links, and a menu button that expands and animates a menu.
+Simple header component that support a logo, a list of links, social media icons, and a menu button that expands and animates a menu.
 
 Uses the BEM naming convention (http://getbem.com/) for class names.
-
-SVG icons shamelessly stolen from font-awesome (https://fontawesome.com/). Replace these.
 
 ### Author
 
@@ -13,10 +11,59 @@ Thomas Christoffersen
 ### Dependencies
 
 - `classnames`
+- `prop-types`
 - `react-tiny-transition`
 
 ### Usage
 
 ```jsx
-<Header />
+<Header header={
+    logo: {
+      src: './assets/logo.png',
+      alt: 'company logo',
+      href: '#take-me-home'
+    },
+    menuButton: {
+      iconUrl: './assets/menu-button.svg',
+      activeIconUrl: './assets/menu-button-active.svg',
+      text: 'Menu'
+    },
+    links: [
+      { text: 'About', href: '#header-link1' },
+      { text: 'Headers', href: '#header-link2' },
+      { text: 'Mockup Masterclass', href: '#header-link3' }
+    ],
+    menu: {
+      navigationLinks: [
+        {
+          heading: 'Who',
+          links: [
+            { text: 'Navigation', href: '#menu-link-1-1' },
+            { text: 'Crowbar', href: '#menu-link-1-2' }
+          ]
+        },
+        {
+          heading: 'What',
+          links: [
+            { text: 'Lobster', href: '#menu-link-2-1' }
+          ]
+        },
+        {
+          heading: 'Where',
+          links: [
+            { text: 'Business', href: '#menu-link-3-1' }
+          ]
+        }
+      ],
+      socialMediaLinks: [
+        {
+          iconUrl: './assets/facebook.svg',
+          href: '#facebook'
+        },
+        {
+          iconUrl: './assets/instagram.svg',
+          href: '#instagram'
+        }
+      ]
+  }}/>
 ```
--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,11 +1,36 @@
-import React from "react";
-import cn from "classnames";
-import TinyTransition from "react-tiny-transition";
+import React from 'react';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+import TinyTransition from 'react-tiny-transition';
 
-import MainMenu from "./main-menu";
+import Menu from './menu';
 
 class Header extends React.Component {
-  state = { isMenuOpen: false };
+  static propTypes = {
+    logo: PropTypes.shape({
+      alt: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired
+    }).isRequired,
+    menuButton: PropTypes.shape({
+      iconUrl: PropTypes.string.isRequired,
+      activeIconUrl: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired,
+    links: PropTypes.arrayOf(
+      PropTypes.shape({
+        href: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired
+      })
+    ),
+    menu: PropTypes.object.isRequired
+  };
+
+  static defaultProps = {
+    links: []
+  };
+
+  state = { isMenuOpen: true };
 
   handleMenuButtonClick() {
     this.setState({
@@ -16,36 +41,43 @@ class Header extends React.Component {
   render() {
     return (
       <div
-        className={cn("header", {
-          "header--is-expanded": this.state.isMenuOpen
+        className={cn('header', {
+          'header--is-expanded': this.state.isMenuOpen
         })}
       >
         <div className="header__toolbar">
-          <a className="header__logo" href="#home" />
+          <a className="header__logo" href={this.props.logo.href}>
+            <img src={this.props.logo.src} alt={this.props.logo.alt} />
+          </a>
           <ul className="header__links">
-            <li className="header__link">
-              <a href="#about">About</a>
-            </li>
-            <li className="header__link">
-              <a href="#headers">Headers</a>
-            </li>
-            <li className="header__link">
-              <a href="#mockup">Mockup Masterclass</a>
-            </li>
+            {this.props.links.map((link, index) => (
+              <li key={index} className="header__link">
+                <a href={link.href}>{link.text}</a>
+              </li>
+            ))}
           </ul>
           <button
             onClick={() => this.handleMenuButtonClick()}
-            className={cn("header__menu-button", {
-              "header__menu-button--is-active": this.state.isMenuOpen
+            className={cn('header__menu-button', {
+              'header__menu-button--is-active': this.state.isMenuOpen
             })}
+            style={{
+              backgroundImage: `url(${
+                this.state.isMenuOpen
+                  ? this.props.menuButton.activeIconUrl
+                  : this.props.menuButton.iconUrl
+              })`
+            }}
           >
-            <span className="header__visually-hidden">MENU</span>
+            <span className="header__visually-hidden">
+              {this.props.menuButton.text}
+            </span>
           </button>
         </div>
 
         {this.state.isMenuOpen && (
           <TinyTransition duration={500}>
-            <MainMenu />
+            <Menu {...this.props.menu} />
           </TinyTransition>
         )}
       </div>
