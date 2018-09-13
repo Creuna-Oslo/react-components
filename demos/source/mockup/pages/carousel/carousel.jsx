6d4222d38217034d87f367694ba4711881f1cdb4
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-09-13 17:07:30 +0200

    Fixes carousel mock

--- demos/source/mockup/pages/carousel/carousel.jsx
+++ demos/source/mockup/pages/carousel/carousel.jsx
@@ -1,31 +1,31 @@
 // Carousel
-import React from 'react';
-import Layout from '../../layout';
+import React from "react";
+import Layout from "../../layout";
 
-import content from './carousel.json';
+import content from "./carousel.json";
 
-import Carousel from '../../../../../components/carousel';
+import Carousel from "components/carousel";
+
+require("components/carousel/carousel.scss");
 
 const CarouselPage = () => {
   const thumbnails = content.map(item => (
     <div key={item.id}>
-      <img src={item.image} style={{ width: '100%' }} />
+      <img src={item.image} style={{ width: "100%" }} />
       <p>{item.title}</p>
     </div>
   ));
 
   return (
     <Layout>
-      <div>
-        <Carousel thumbnails={thumbnails}>
-          {content.map(item => (
-            <div key={item.id}>
-              <h2>{item.title}</h2>
-              <img src={item.image} />
-            </div>
-          ))}
-        </Carousel>
-      </div>
+      <Carousel thumbnails={thumbnails}>
+        {content.map(item => (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <img src={item.image} />
+          </div>
+        ))}
+      </Carousel>
     </Layout>
   );
 };
