67d919be61ad5b52b23a01598640dc7d3b9d5b19
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-09-13 17:07:01 +0200

    Adds some styling to layout

--- demos/source/mockup/pages/home.jsx
+++ demos/source/mockup/pages/home.jsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+
+import Layout from "../layout";
 
 const Home = ({ pageGroups }) => (
-  <div className="home">
-    <style>{`
+  <Layout showLink={false}>
+    <div className="home">
+      <style>{`
       .home {
         display: flex;
         flex-wrap: wrap;
       }
     `}</style>
-    <h1 className="name" style={{ width: '100%' }}>
-      Creuna React components
-    </h1>
-    {pageGroups.map(group => (
-      <div key={group.title} style={{ width: 300 }}>
-        <p style={{ margin: 0 }}>{group.title}</p>
-        <ul
-          style={{
-            listStyle: 'none',
-            padding: 0,
-            marginBottom: 40,
-            marginTop: 10
-          }}
-        >
-          {group.pages.map(page => (
-            <li key={page.url}>
-              <a href={page.url} style={{ color: 'black' }}>
-                {page.name || page.url}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    ))}
-  </div>
+      <h1 className="name" style={{ width: "100%" }}>
+        Creuna React components
+      </h1>
+      {pageGroups.map(group => (
+        <div key={group.title} style={{ width: 300 }}>
+          <p style={{ margin: 0 }}>{group.title}</p>
+          <ul
+            style={{
+              listStyle: "none",
+              padding: 0,
+              marginBottom: 40,
+              marginTop: 10
+            }}
+          >
+            {group.pages.map(page => (
+              <li key={page.url}>
+                <a href={page.url} style={{ color: "black" }}>
+                  {page.name || page.url}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  </Layout>
 );
 
 Home.propTypes = {
